f2ae76e5afb95f6d0522e73cbb814207c642163a
Steph Hjartarson <stephanie.hja@gmail.com>
2022-11-06 14:11:23 +0000

    Prepared to deploy to Heorku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +77,20 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://fqouczoylpfu'
+                                     'mc:daf368156d8897d8c53a'
+                                     '28e940b2f56ae62a0941f853'
+                                     '94058922cf77748efedc@ec2-3-219-135-162.'
+                                     'compute-1.amazonaws.com:5432/'
+                                     'd7n2h811jp2shl')
 }
 
 
